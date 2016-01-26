23893dbcda4ade472c2da378c80b14a6564d2f19
Manginelli <daniel.manginelli.o@gmail.com>
2016-01-25 23:01:33 -0300

    0.2
    with 2 levels!

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class Score : MonoBehaviour {
 
     static private int s = 0;
+    static private bool changedScene = false;
+    private Timer t;
+
     static public int score
     {
         get
         {
             return s;
         }
+    }
+
+    void Start()
+    {
+        t = new Timer();
+    }
+
+    void Update()
+    {
+        if (score == 5 && !changedScene)
+        {
+            changedScene = true;
+            Debug.Log(changedScene);
+            t.SetTimer(2f);
+            Debug.Log(t.TimeOver());
+            GameManagement.Stop();
+            
+        }
+        if (changedScene && t.TimeOver())
+        {
+            IncreaseScore();
+            changedScene = false;
+            SceneManager.LoadScene("Scene1");
+            GameManagement.Reset();
+        } 
     }
 
     static public void IncreaseScore()
